6bcd73f970917dff79ad94b3b0c4c4a81f7a9e1b
wwnj <505230405@qq.com>
2026-01-19 14:08:21 +0800

    disable mobile adaptation

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,15 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
-    'postcss-px-to-viewport': {
-      viewportWidth: 375,        // 设计稿宽度
-      viewportUnit: 'vw',        // 转换单位
-      selectorBlackList: ['.el-'], // Element Plus 不转换
-      minPixelValue: 1,
-      mediaQuery: false,
-      exclude: [/node_modules/], // 排除 node_modules
-    },
+    // 注释掉 postcss-px-to-viewport，因为这是桌面端管理系统
+    // 如果需要移动端适配，可以取消注释并调整配置
+    // 'postcss-px-to-viewport': {
+    //   viewportWidth: 1920,       // 桌面端设计稿宽度
+    //   viewportUnit: 'vw',        // 转换单位
+    //   selectorBlackList: ['.el-'], // Element Plus 不转换
+    //   minPixelValue: 1,
+    //   mediaQuery: false,
+    //   exclude: [/node_modules/], // 排除 node_modules
+    // },
   },
 }
